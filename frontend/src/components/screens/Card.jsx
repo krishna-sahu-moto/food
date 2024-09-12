701efd0531a4d70ba6720a07e00c52fb380ae570
krishna-sahu-moto <sankisahumoto16@gmail.com>
2024-09-12 16:52:49 +0530

    Update Card.jsx

--- frontend/src/components/screens/Card.jsx
+++ frontend/src/components/screens/Card.jsx
@@ -38,7 +38,8 @@ export default function Card() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     console.log(fetched_data, localStorage.getItem("userEmail"), new Date(), "anil")
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch(" https://food-backend-l0nx.onrender.com", {
+     // http://localhost:5000/api/orderData
       // credentials: 'include',
       // Origin:"http://localhost:5173/login",
 
