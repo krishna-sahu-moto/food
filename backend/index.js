6c302ae050ff9ffb01f51d61a87c8468a087e518
krishna-sahu-moto <sankisahumoto16@gmail.com>
2024-09-12 16:03:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoDB = require('./db')
 const cors = require('cors');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 mongoDB();
 
 app.use((req,res,next)=>{
